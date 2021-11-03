1c22801a1680abdaadc1fbe2746f0354a3f80140
Chinuster <84125005+Chinuster@users.noreply.github.com>
2021-11-03 14:37:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,96 +1,62 @@
-canvas=document.getElementById("myCanvas");
-ctx=canvas.getContext("2d");
 
-mars_images=["image_1.jpg","image_2.jpg","image_3.jpg","image_4.jpg"];
-random_number=Math.floor(Math.random()* 4);
-console.log(random_number);
-background_img="mars.jpg";
-rover_width=100;
-rover_height=90;
-background_image=mars_images[random_number];
-console.log("background_image="+background_image);
-rover_image="rover.png";
-rover_x=10;
-rover_y=10;
+// Create canvas variable
+var canvas = new fabric.Canvas('myCanvas');
+ block_y=1;
+ block_x=1;
 
+block_image_width = 350;
+block_image_height = 430;
 
+var block_image_object= "";
 
-function add(){
-   background_imgTag=new Image();
-    background_imgTag.onload = uploadBackground;
-background_imgTag.src=background_image;
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(Img){
+		block_image_object = Img;
 
-rover_imgTag=new Image();
-rover_imgTag.onload = uploadRover;
-rover_imgTag.src=rover_image;
+		block_image_object.scaleToWidth(block_image_width);
+		block_image_object.scaleToHeight(block_image_height);
+		block_image_object.set({
+			top:block_y,
+			left:block_x
+		});
+		canvas.add(block_image_object);
+	});
 
+	}
 
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // add appropriate keycode
+	{
+		new_image('rr1.png');
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		new_image('gr.png');
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		new_image('yr.png');
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		new_image('pr.png');
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+		new_image('br.png');
+	}
+	
 }
 
-
-function uploadBackground(){
-    ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
-
-}
-
-function uploadRover(){
-    ctx.drawImage(rover_imgTag,rover_x,rover_y,rover_width,rover_height);
-
-}
-window.addEventListener("keydown",my_keydown);
-function my_keydown(e){
-    keyPressed=e.keyCode;
-    console.log(keyPressed);
-
-    if (keyPressed=='37')
-    {
-        left();
-        console.log("left");
-    }
-    if (keyPressed=='38')
-    {
-        up();
-        console.log("up");
-    }
-    if (keyPressed=='39')
-    {
-        right();
-        console.log("right");
-    }
-    if (keyPressed=='40')
-    {
-        down();
-        console.log("down");
-    }
-}
-
-function left(){
-    if(rover_x >=0){
-        rover_x=rover_x-10;
-        console.log("When left arrow is pressed, x = " + rover_x + " | y = " +rover_y);
-        uploadBackground();
-        uploadRover();
-    }
-}
-function right(){
-    if(rover_x <=700){
-        rover_x=rover_x+10;
-        console.log("When right arrow is pressed, x = " + rover_x + " | y = " +rover_y);
-        uploadBackground();
-        uploadRover();
-    }
-}function up(){
-    if(rover_y >=0){
-        rover_y=rover_y-10;
-        console.log("When up arrow is pressed, x = " + rover_x + " | y = " +rover_y);
-        uploadBackground();
-        uploadRover();
-    }
-}function down(){
-    if(rover_y <=500){
-        rover_y=rover_y+10;
-        console.log("When down arrow is pressed, x = " + rover_x + " | y = " +rover_y);
-        uploadBackground();
-        uploadRover();
-    }
-}
